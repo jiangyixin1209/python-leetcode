ba6fedf263b41fd2181f33cd5c0b3ecb828403d7
jiangyx3915 <jyx15221613915@gmail.com>
2020-11-18 17:42:29 +0800

    001

---
+++ 001.两数之和.py
@@ -0,0 +1,37 @@
+# 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。 
+# 
+#  你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。 
+# 
+#  
+# 
+#  示例: 
+# 
+#  给定 nums = [2, 7, 11, 15], target = 9
+# 
+# 因为 nums[0] + nums[1] = 2 + 7 = 9
+# 所以返回 [0, 1]
+#  
+#  Related Topics 数组 哈希表 
+#  👍 9639 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        cache = {}
+        for i, v in enumerate(nums):
+            if target - v in cache:
+                return [cache[target - v], i]
+            cache[v] = i
+        return [-1, -1]
+
+
+# leetcode submit region end(Prohibit modification and deletion)
+"""
+遍历数组缓存当前值和索引，每次将target与当前值相减如果在缓存中则返回
+"""
